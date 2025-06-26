29988c85bc707f6a994f9bfb4c571be491a33c1b
Devendra Bankey <devendrabankey48199@gmail.com>
2025-06-26 18:20:59 +0530

    testing console for api base url

--- src/services/authService.js
+++ src/services/authService.js
@@ -7,7 +7,7 @@ const identifyMe = async () => {
 };
 const loginUser = async (data) => {
   const response = await api.post("/user/login", data);
-  // console.log("working");
+  console.log(import.meta.env.VITE_API_BASE_URL);
   return response.data;
 };
 const logoutUser = async () => {

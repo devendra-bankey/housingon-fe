7d5554cb0b760cda33053a36bfd96c9d9fa6cd97
Devendra Bankey <devendrabankey48199@gmail.com>
2025-06-26 17:04:00 +0530

    change env var naming with VITE

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: import.meta.env.API_BASE_URL,
-  baseURL: "http://localhost:6500",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true, // for cookie purpose
 });
 
